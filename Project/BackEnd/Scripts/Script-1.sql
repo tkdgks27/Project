c10b7f4fb4cdbf620470b0bb2c38e89e4d351125
minho <152574713+ryouminho@users.noreply.github.com>
2023-12-13 10:22:20 +0900

    수정

---
+++ Project/BackEnd/Scripts/Script-1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE img_ryou(
+	i_title varchar2(30 char) PRIMARY KEY,
+	i_filename varchar2(100 char) NOT NULL
+);
+
+SELECT * FROM img_ryou;
---
+++ Project/fileUpload.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const FileUpload = () => {
+  return <>
+    
+    <h2>게시판</h2>
+  
+    <Link to="/">홈으로</Link>
+  </>;
+};
+
+
+export default FileUpload;
