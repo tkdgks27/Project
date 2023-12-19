e9d60c3720b183799b896da0dcc2b2a40166b31b
dkeh20 <dkeh20@naver.com>
2023-12-19 16:58:18 +0900

    test

--- FrontEnd/project_1/src/mainmenu/BoardLists.js
+++ FrontEnd/project_1/src/mainmenu/BoardLists.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const bodyStyle = {
     backgroundColor: "#17171e",
@@ -9,18 +10,37 @@ const bodyStyle = {
 };
 
 const BoardList = () => {
+  const navigate = useNavigate();
+  const [BoardList, setBoardList] = useState([]);
+
+  const getBoardList = async () => {
+    const response = await (await axios.get("http://localhost:3001/write.do")).data
+    setBoardList(response.data);
+
+    const pngn = response.pagination;
+    console.log(pngn);
+  };
+
+  const MoveToWrite = () => {
+    navigate('/boardwrite');
+  };
+
+  useEffect(() => {
+    getBoardList();
+  }, []);
+
   return (
     <div style={bodyStyle}>
-    <h2>게시판</h2>
-    제목 : <input /><p />
-    파일 : <input type='file' /><p />
-    <button>업로드</button>
-    <hr />
-    <table border={1}></table>
-    <br />
-    <Link to="/boardwrite">
-    <button>글쓰기</button>
-    </Link>
+    <div>
+      <ul>
+        {BoardList.map((board) => (
+          <li key={board.idx}>
+            <Link to ={'/board/${board.idx}'}>{board.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <button onClick={MoveToWrite}>글쓰기</button>
     </div>
   );
 };
