3e8eb4d855ef3291ccaf023cb033f144f1b0a8c2
dkeh20 <dkeh20@naver.com>
2023-12-23 09:12:14 +0900

    test

---
+++ BackEnd/src/test/java/com/cip/Member/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cip.Member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
